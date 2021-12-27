fe6180420df2043993c2c58171a834663448b3c4
5lunk <desaccorde@gmail.com>
2021-12-27 12:09:58 +0000

    tabs

--- syslog_filter.sql
+++ syslog_filter.sql
@@ -2,13 +2,13 @@
 -- function black_hole doesn't exist
 DROP TRIGGER IF EXISTS `Syslog`.`filter_fan`;
 DELIMITER //
-    CREATE TRIGGER `Syslog`.`filter_fan`
-    BEFORE INSERT
-    ON `Syslog`.`SystemEvents`
-    FOR EACH ROW
-    BEGIN
-        IF new.Message LIKE '%FAN_FAILED: Fan 2 not rotating' THEN
-            CALL black_hole();
-        END IF;
-    END//
+	CREATE TRIGGER `Syslog`.`filter_fan`
+	BEFORE INSERT
+	ON `Syslog`.`SystemEvents`
+	FOR EACH ROW
+	BEGIN
+		IF new.Message LIKE '%FAN_FAILED: Fan 2 not rotating' THEN
+			CALL black_hole();
+		END IF;
+	END//
 DELIMITER ;
